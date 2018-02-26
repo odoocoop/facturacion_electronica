31dd08033d9074f58f5c03ccf983d85aec8ede8b
Daniel Santibáñez Polanco <dansanti@gmail.com>
2018-02-26 07:58:33 -0300

    Cambios Acumulados a cesión de créditos

--- l10n_cl_dte_factoring/__manifest__.py
+++ l10n_cl_dte_factoring/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": """Cesión de Créditos Electrónica para Chile (factoring)\
     """,
-    'version': '11.0.5.1.0',
+    'version': '1.1.2',
     'category': 'Localization/Chile',
     'sequence': 12,
     'author':  'Daniel Santibáñez Polanco, Cooperativa OdooCoop',
