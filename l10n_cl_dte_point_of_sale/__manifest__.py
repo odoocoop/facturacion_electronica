09e57858d365d76ef242cef9d1893a6765ddb96d
dansanti <dansanti@gmail.com>
2019-06-10 00:29:10 -0400

    [FIX]Downloader Boleta

--- l10n_cl_dte_point_of_sale/__manifest__.py
+++ l10n_cl_dte_point_of_sale/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": """Boleta / Factura Electrónica Chilena para punto de ventas \
     """,
-    'version': '0.12.1',
+    'version': '0.12.2',
     'category': 'Point of Sale/Localization/Chile',
     'sequence': 12,
     'author':  'Daniel Santibáñez Polanco, Cooperativa OdooCoop',
