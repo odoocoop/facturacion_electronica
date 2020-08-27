a35dea1f472d934bea9d0a91a8ba1387523e0445
Daniel Santibáñez Polanco <dansanti@gmail.com>
2020-08-27 06:51:34 -0400

    Cambios acumulados

--- l10n_cl_fe/models/sale_order.py
+++ l10n_cl_fe/models/sale_order.py
@@ -37,3 +37,42 @@ class SO(models.Model):
                 )
         return vals
 
+    @api.depends('order_line.price_total')
+    def _amount_all(self):
+        """
+        Compute the total amounts of the SO.
+        """
+        for order in self:
+            amount_untaxed = amount_tax = 0.0
+            for line in order.order_line:
+                amount_untaxed += line.price_subtotal
+                amount_tax += line.price_tax
+            amount_untaxed = order.currency_id.round(amount_untaxed)
+            amount_tax = order.currency_id.round(amount_tax)
+            order.update({
+                'amount_untaxed': amount_untaxed,
+                'amount_tax': amount_tax,
+                'amount_total': amount_untaxed + amount_tax,
+            })
+
+
+
+class SOL(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
+    def _compute_amount(self):
+        """
+        Compute the amounts of the SO line.
+        """
+        for line in self:
+            taxes = line.tax_id.compute_all(
+                line.price_unit, line.order_id.currency_id,
+                line.product_uom_qty, product=line.product_id,
+                partner=line.order_id.partner_shipping_id,
+                discount=line.discount, uom_id=line.uom_id)
+            line.update({
+                'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
+                'price_total': taxes['total_included'],
+                'price_subtotal': taxes['total_excluded'],
+            })
